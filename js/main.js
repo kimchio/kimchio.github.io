8387b449c4607953afed99aa3f02a4b1aafabe13
kimchio <idycon@naver.com>
2016-08-06 09:59:35 +0900

    test

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-document.getElementById('test').style.color='blue'
+var jsonObject = new Object();
+jsonObject.sideName = 'cuda';
+jsonObject.subject = 'cuda이용하기';
+jsonObject.context = 'what the test!';
+
+var text = JSON.stringify(jsonObject);
+
+function test(x){
+	m = document.getElementById('side').childNodes[1];
+	
+	attacha = document.createElement('a');
+	attacha.href = '#what';
+	attacha.textContent = 'add';
+	li1 = document.createElement('li');
+	li1.appendChild(attacha);
+	m.appendChild(li1);
+
+}
